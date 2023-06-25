0b5c4b6f67eafa6487eb0fc30df95a015d1b20ef
taccisum <liaojinfeng6937@dingtalk.com>
2023-06-25 09:49:01 +0800

    +: sample usages for map & slice

--- map.go
+++ map.go
@@ -9,4 +9,13 @@ func main() {
 	}
 
 	fmt.Println(dict)
+	v, ok := dict["foo"]
+	if ok {
+		fmt.Println("foo exists in map. value", v)
+	}
+
+	_, ok = dict["taccisum"]
+	if !ok {
+		fmt.Println("taccisum does not exist in map.")
+	}
 }
--- slice.go
+++ slice.go
@@ -4,9 +4,10 @@ import "fmt"
 
 func main() {
 	ls := []int {1,2,3}
-	fmt.Println(ls)
+	fmt.Println("slice", ls)
 	ls = append(ls, 4)
-	fmt.Println(ls)
-	fmt.Println(ls[1:])
-	fmt.Println(ls[:3])
+	fmt.Println("append 4", ls)
+	fmt.Println("slice[1:]", ls[1:])
+	fmt.Println("slice[:3]", ls[:3])
+	fmt.Println("slice len", len(ls), "cap", cap(ls))
 }
